59bea5b466f7f2a0ba2ecd11813747ccd60bbb71
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-01-18 14:26:54 -0500

    create component for button and add class types for confirm and danger

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
+export default function Button({confirm, danger, disabled, children}) {
+   let buttonClass = "button";
+   if(confirm) {
+      buttonClass += " button--confirm";
+   } else if(danger) {
+      buttonClass += " button--danger";
+   }
+   return (
+      <button className={buttonClass}>{children}</button>
+   );
 }
