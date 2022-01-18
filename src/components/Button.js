2c1d10a41f382e52ec7f21a5860eeb32408688d3
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-01-18 14:56:20 -0500

    add onClick and disabled attributes to button if present in props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import React, { Fragment } from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions";
 
-export default function Button({confirm, danger, disabled, children}) {
+export default function Button({confirm, danger, clickable, onClick, disabled, children}) {
    let buttonClass = "button";
    if(confirm) {
       buttonClass += " button--confirm";
    } else if(danger) {
       buttonClass += " button--danger";
    }
+
    return (
-      <button className={buttonClass}>{children}</button>
+      <button onClick={onClick} className={buttonClass} disabled={disabled}>{children}</button>
    );
 }
