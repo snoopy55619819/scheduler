7672f0746e86f9ad01a8cc5c3198d33f22cecd10
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-01-18 19:15:41 -0500

    create Button and cli tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,22 @@ import React, { Fragment } from "react";
 
 import "components/Button.scss";
 import { action } from "@storybook/addon-actions";
+import classNames from "classnames";
 
-export default function Button({confirm, danger, clickable, onClick, disabled, children}) {
-   let buttonClass = "button";
-   if(confirm) {
-      buttonClass += " button--confirm";
-   } else if(danger) {
-      buttonClass += " button--danger";
-   }
-
+export default function Button({confirm, danger, onClick, disabled, children}) {
+   const buttonClass = classNames({
+      "button": true,
+      "button--confirm": confirm,
+      "button--danger": danger
+   });
+   
    return (
-      <button onClick={onClick} className={buttonClass} disabled={disabled}>{children}</button>
+      <button 
+         onClick={onClick} 
+         className={buttonClass} 
+         disabled={disabled}
+      >
+         {children}
+      </button>
    );
 }
