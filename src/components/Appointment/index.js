be5f78be0b23e433fe4936e5a88ddf52d05a0518
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-01-21 21:44:36 -0500

    import and use custom hook to modify mode of form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,34 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
-export default function Appointment({ time, interview }) {
+export default function Appointment({ time, interview, interviewers }) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  
+  let { mode, transition, back } = useVisualMode((interview ? SHOW : EMPTY));
 
   return (
     <article className="appointment">
       <Header time={time} />
-      { interview ? <Show student={interview.student} interviewer={interview.interviewer.name}/> : <Empty /> }
+      { (mode === "EMPTY") && <Empty onAdd={() => transition(CREATE)}/> }
+      { (mode === "SHOW") && 
+        <Show 
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />
+      }
+      {/* props: { student, interviewer, interviewers, onSave, onCancel } */}
+      { (mode === "CREATE") && 
+        <Form 
+          interviewers={interviewers}
+          onCancel={() => back()}
+        />
+      }
+      
     </article>
   );
 }
