a7ad30e017077a3a29e54d6ca2902f52addd3d7a
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-01-31 16:15:59 -0500

    refactor DayList.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,15 +2,16 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList({ days, value, onChange }) {
-  const listOfDaysData = days.map(currDay => {
-    return <DayListItem
-      key={currDay.id}
-      name={currDay.name}
-      spots={currDay.spots}
-      selected={currDay.name === value}
-      setDay={() => onChange(currDay.name)}
+  const listOfDaysData = days.map(({ id, name, spots }) => 
+    <DayListItem
+      key={id}
+      name2
+      name={name}
+      spots={spots}
+      selected={name === value}
+      setDay={() => onChange(name)}
     />
-  });
+  );
 
   return(
     <ul>
